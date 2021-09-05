0036a17ae7dc9c2228c4a4bf534f2429d6aaefb4
Catherine Holloway <milankie@gmail.com>
2021-09-05 13:58:43 -0400

    removing some lookups

--- livarot_pybind.cpp
+++ livarot_pybind.cpp
@@ -2,18 +2,19 @@
 #include <2geom/svg-path-parser.h>
 #include <2geom/path-sink.h>
 #include "livarot/Shape.h"
+#include "livarot/Path.h"
 
 
 namespace py = pybind11;
 
 
-PYBIND11_MODULE(livarot, m) {
+PYBIND11_MODULE(pylivarot, m) {
     m.doc() = "python bindings to the functionality within livarot, with some helper classes from 2geom and inkscape"; 
     py::class_<Geom::PathVector>(m, "PathVector");
     py::class_<Geom::PathSink>(m, "PathSink");
-    py::class_<Geom::Path>(m, "Path")
-    .def(py::init<>());
-    //.def("LoadPathVector", &Geom::Path::LoadPathVector);
+    py::class_<Path>(m, "Path")
+    .def(py::init<>())
+    .def("LoadPathVector", py::overload_cast<Geom::PathVector const &>(&Path::LoadPathVector));
 
     py::class_<Geom::PathBuilder, Geom::PathSink>(m, "PathBuilder")
     .def(py::init<>());
