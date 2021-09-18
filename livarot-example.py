7054249a2756b1b20d5890c4d3b31d1a09c61b69
Catherine Holloway <milankie@gmail.com>
2021-09-18 13:35:18 -0400

    starting to organize python and c++ code simultaneously

--- livarot-example.py
+++ livarot-example.py
@@ -2,7 +2,7 @@ import sys
 from os.path import dirname, join
 sys.path.append(join(dirname(__file__), "linux-build"))
 
-import pylivarot as livarot
+import _pylivarot as livarot
 
 shape = livarot.Shape()
 d_string = "M 140.60714,82.398807 112.12372,76.862861 91.415657,97.188406 87.878796,68.388367 62.14891,54.974767 88.446424,42.71131 93.252544,14.095704 113.04216,35.31651 141.74239,31.044693 127.67554,56.423329 Z"
--- livarot_pybind.cpp
+++ livarot_pybind.cpp
@@ -8,10 +8,19 @@
 namespace py = pybind11;
 
 
-PYBIND11_MODULE(pylivarot, m) {
+PYBIND11_MODULE(_pylivarot, m) {
     m.doc() = "python bindings to the functionality within livarot, with some helper classes from 2geom and inkscape"; 
     py::class_<Geom::PathVector>(m, "PathVector");
     py::class_<Geom::PathSink>(m, "PathSink");
+
+    py::class_<Shape>(m, "Shape")
+    .def(py::init<>())
+    .def("getPoint", &Shape::getPoint)
+    .def("hasBackData", &Shape::hasBackData)
+    .def("ConvertToShape", &Shape::ConvertToShape)
+    .def("getEdge", &Shape::getEdge)
+    .def("Booleen", &Shape::Booleen);
+
     py::class_<Path>(m, "Path")
     .def(py::init<>())
     .def("LoadPathVector", py::overload_cast<Geom::PathVector const &>(&Path::LoadPathVector))
@@ -33,9 +42,6 @@ PYBIND11_MODULE(pylivarot, m) {
     .def(py::init<Geom::PathSink &>())
     .def("setZSnapThreshold", &Geom::SVGPathParser::setZSnapThreshold)
     .def("parse", py::overload_cast<std::string const &>(&Geom::SVGPathParser::parse));
-    py::class_<Shape>(m, "Shape")
-    .def(py::init<>())
-    .def("Booleen", &Shape::Booleen);
 
     m.def("parse_svg_path", py::overload_cast<char const *>(&Geom::parse_svg_path));
 
